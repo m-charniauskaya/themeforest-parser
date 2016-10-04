489754596e88282cdfc93dd5c9e1bb377ff1ab62
masha <byseledka@gmail.com>
2016-10-04 21:06:49 +0300

    parser.php

--- parser.php
+++ parser.php
@@ -1,11 +1,10 @@
 <?php
   $file = file_get_contents('https://agileui.com/demo/delight/demo/admin-template/index.html');
-  if (file_exists(__DIR__.'/downloads') != true){
-    $downloads = mkdir (__DIR__.'/downloads');
+  $downloadDir = 'downloads';
+  if (file_exists(__DIR__.'/'.$downloadDir) != true){
+    mkdir (__DIR__.'/downloads');
   }
-  $dh = opendir ($downloads);
-  $copy = fopen('index.html', 'a+');
+  $copy = fopen(__DIR__.'/'.$downloadDir.'/index.html', 'a+');
   fwrite($copy, $file);
-  closedir($dh);
-  var_dump($downloads);
+  echo $copy;
 ?>

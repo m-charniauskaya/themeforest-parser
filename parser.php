6df67ad9e3a48f3d70a9752073e86b1ca3bc817e
masha <byseledka@gmail.com>
2016-10-11 19:52:09 +0300

    parser.php

--- parser.php
+++ parser.php
@@ -37,14 +37,46 @@
 
       $links_js = array();
       foreach ($dom->getElementsByTagName('script') as $link_js){
-        $links_js[] = $link_js->getAttribute('src');
-        $name_js = substr($link_js->getAttribute('src'), 6);
-        $link_js->setAttribute('src', './'.$name_js);
+        if(!empty($link_js->getAttribute('src'))){
+          $links_js[] = $link_js->getAttribute('src');
+          $name_js = substr($link_js->getAttribute('src'), 6);
+          $link_js->setAttribute('src', './'.$name_js);
+        }
       }
 
       foreach ($links_js as $link){
         download_save_file($link, __DIR__.'/'.$downloadDir.'/'.substr($link, 6));
       }
 
+      $links_img = array();
+      foreach ($dom->getElementsByTagName('link') as $link_img){
+        if($link_img->getAttribute('rel') != 'stylesheet'){
+          $links_img[] = $link_img->getAttribute('href');
+          $name_img = substr($link_img->getAttribute('href'), 6);
+          $link_img->setAttribute('href', './'.$name_img);
+        }
+      }
+
+      foreach ($dom->getElementsByTagName('img') as $link_img){
+          $links_img[] = $link_img->getAttribute('src');
+          $name_img = substr($link_img->getAttribute('src'), 6);
+          $link_img->setAttribute('src', './'.$name_img);
+      }
+
+      foreach ($links_img as $link){
+        $download_img = 'https://agileui.com/demo/delight/'.substr($link, 6);
+        $folders = explode('/', substr($link, 6));
+        $pop = array_pop($folders);
+        $i = '';
+
+        foreach ($folders as $folder){
+          $i = $i.$folder.'/';
+          if (!is_dir(__DIR__.'/'.$downloadDir.'/'.$i)){
+            mkdir(__DIR__.'/'.$downloadDir.'/'.$i);
+          }
+        }
+        copy ($download_img, __DIR__.'/'.$downloadDir.'/'.$i.$pop);
+      }
+      @$dom->saveHTMLFile(__DIR__.'/'.$downloadDir.'/'.substr($link_html,6));
 
 ?>
